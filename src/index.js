c25978442139f4026949defbc181ce71273b86a2
sivasankar <manusankar410@gmail.com>
2019-12-09 16:24:37 +0530

    chore:welcome

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 import './style.css';
+import Welcome from './welcome';
 
 
-const App = () => <div>Welcome hello world {React.version}</div>
+const App = () =>
+    <div>
+        Welcome hello world {React.version}
+        <Welcome />
+    </div>
 
 
 render(<App />, document.getElementById('root'));
---
+++ src/welcome.js
@@ -0,0 +1,4 @@
+import React from 'react'
+
+const Welcome = () => <div>Hello manasa</div>
+export default Welcome;
