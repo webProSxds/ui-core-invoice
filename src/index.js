b48a715a76933d79c2944bc920fa8707f7a582e3
sivasankar <manusankar410@gmail.com>
2019-12-09 12:14:18 +0530

    chore:hello

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import './style.css';
 
 
-const App = () => <div>Welcome {React.version}</div>
+const App = () => <div>Welcome hello world {React.version}</div>
 
 
 render(<App />, document.getElementById('root'));
