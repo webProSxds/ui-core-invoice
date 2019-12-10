874f6f83c1c17e187369268e5a6ed0867952f37f
sivasankar <manusankar410@gmail.com>
2019-12-10 11:25:11 +0530

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import './style.css';
-
-
-const App = () => <div>Welcome {React.version}</div>
-
-
-render(<App />, document.getElementById('root'));
