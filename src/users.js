9b8de29c6b91114db9539860be63ae1f8934c0d6
sivasankar <manusankar410@gmail.com>
2019-12-17 15:45:46 +0530

    updated

--- src/users.js
+++ src/users.js
@@ -1,11 +1,66 @@
-import React from 'react';
-import learn from './learn-ui';
+import React, { useState } from 'react';
 
+
+const initData = {
+	items: [
+		{ name: 'sivas', id:1},
+		{ name: 'Manasasiva', id:2 },
+		{ name: 'sankar', id:3 },
+	],
+	selecteItem: ''
+}
 export default () => {
-	const { Welcome } = learn;
+	const [data, updateData] = useState(initData);
+
+	const updateHandler = (a) =>{
+		updateData({...data, selecteItem:a})
+	}
+	
+	const clickCapitaHandler = ()=>{
+		console.log("clickCapitaHandler",data.selecteItem);
+
+		let ns = data.selecteItem.name.toUpperCase();
+		
+
+
+
+		//fetch the record from data
+		
+		
+		data.items.map(item=>{
+				if(item.id == data.selecteItem.id){
+					item.name = item.name.toUpperCase();	
+
+					return item;
+				}
+			return item;
+		});
+
+		
+		updateData({...data, selecteItem:{...data.selecteItem, name:ns}});
+		
+
+	}
+
+	
+
 	return (
-		<div class="content">
-			<Welcome />
+		<div className="content">
+			{
+				<pre>{JSON.stringify(data,null,2)}</pre>
+			}
+			{
+				data.items.map(
+					(item, id) =>
+						<div className="box" onClick={()=>updateHandler(item)}>
+							{item.name}
+						</div>
+				)
+			}
+
+			{
+				<button onClick={()=>clickCapitaHandler()}>Capital</button>
+			}
 		</div>
 	);
 }
