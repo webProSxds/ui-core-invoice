7e3a1d93678c0efab7c673c0a07cf5c1224fca1a
sivasankar <manusankar410@gmail.com>
2020-01-04 13:38:36 +0530

    Updates

---
+++ precache-manifest.ea8038f257bb43cc2bf5f037aa23b170.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12cced930940eed576c2bf438c5adc08",
+    "url": "/ui-core-invoice/index.html"
+  },
+  {
+    "revision": "d0def58ba75824fe54c4",
+    "url": "/ui-core-invoice/static/css/main.0440fe4e.chunk.css"
+  },
+  {
+    "revision": "09187c8dd39c375d128e",
+    "url": "/ui-core-invoice/static/js/2.17f7dd48.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/ui-core-invoice/static/js/2.17f7dd48.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d0def58ba75824fe54c4",
+    "url": "/ui-core-invoice/static/js/main.21145c5b.chunk.js"
+  },
+  {
+    "revision": "ee201e380b47fb0ef1f7",
+    "url": "/ui-core-invoice/static/js/runtime-main.7f7fc954.js"
+  }
+]);
