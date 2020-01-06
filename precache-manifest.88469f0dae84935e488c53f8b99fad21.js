2a194bbc261e3e973b252fd6b51ae52ecd50fafa
sivasankar <manusankar410@gmail.com>
2020-01-06 11:11:36 +0530

    Updates

--- precache-manifest.88469f0dae84935e488c53f8b99fad21.js
+++ precache-manifest.88469f0dae84935e488c53f8b99fad21.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4a7ac6c7acee511c0edb477638b46a65",
+    "revision": "936b8a5054891a79da6b2b1035693a18",
     "url": "/ui-core-invoice/index.html"
   },
   {
-    "revision": "94f0ca2121c0c9a211a6",
+    "revision": "1656a6bb1ee3dd029906",
     "url": "/ui-core-invoice/static/css/main.b70480c8.chunk.css"
   },
   {
-    "revision": "f41b09139ad86e1a24a5",
-    "url": "/ui-core-invoice/static/js/2.e2c31420.chunk.js"
+    "revision": "f7b9c4ab084702af83eb",
+    "url": "/ui-core-invoice/static/js/2.6cbf11e6.chunk.js"
   },
   {
     "revision": "e928fe768baa9832b5bc57eae021f30c",
-    "url": "/ui-core-invoice/static/js/2.e2c31420.chunk.js.LICENSE"
+    "url": "/ui-core-invoice/static/js/2.6cbf11e6.chunk.js.LICENSE"
   },
   {
-    "revision": "94f0ca2121c0c9a211a6",
-    "url": "/ui-core-invoice/static/js/main.5b780fed.chunk.js"
+    "revision": "1656a6bb1ee3dd029906",
+    "url": "/ui-core-invoice/static/js/main.2bb086df.chunk.js"
   },
   {
     "revision": "ee201e380b47fb0ef1f7",
